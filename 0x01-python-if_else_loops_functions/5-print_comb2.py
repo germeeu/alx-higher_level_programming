76de7a6c5ec01b2696d5fed2733d1382232f9009
Muhammed Jamiu Saka <sakasim_jay@yahoo.com>
2022-11-29 06:00:00 -0800

    task 5 and 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for n in range(100):
     if n != 99:
-        print(f"{n:02d}, ", end='')
-    else:
-        print(f"{n:02d}")
+        print(f"{n:02d}", end='' if n == 99 else ", ")
