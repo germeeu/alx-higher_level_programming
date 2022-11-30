d512bc7372704e6dbc853687cad5241c1b2a2045
Muhammed Jamiu Saka <sakasim_jay@yahoo.com>
2022-11-30 15:51:50 -0800

    updates

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    string = str[: n] + str[n+1:]
-    return(string)
+    new = ""
+    i = 0
+    for c in str:
+        if i != n:
+            new += c
+        i += 1
+    return new
