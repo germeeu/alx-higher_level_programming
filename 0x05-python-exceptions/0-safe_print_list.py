ca1b75dfc39e971ab14e3830325d6bbf662f80b3
Muhammed Jamiu Saka <sakasim_jay@yahoo.com>
2022-12-19 08:49:47 -0800

    readme file and task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    n = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except IndexError:
+            break
+        else:
+            n += 1
+
+    print()
+    return (n)
